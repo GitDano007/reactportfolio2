2a1778d2e83fd1bb79222cc2f280d1d4d97b1d88
Dano Seese <dpseese@yahoo.com>
2020-11-18 15:39:19 -0800

    Updates

---
+++ precache-manifest.ca32de5fe970cb5cff7686d55f3b9c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a584970c85b9b18bf57a5d2f03c765",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "093d5e96caa0d2decef0",
+    "url": "/reactportfolio/static/css/main.d69e5018.chunk.css"
+  },
+  {
+    "revision": "f2b21fa9d4a77811cf92",
+    "url": "/reactportfolio/static/js/2.3b9c633b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactportfolio/static/js/2.3b9c633b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "093d5e96caa0d2decef0",
+    "url": "/reactportfolio/static/js/main.e1981dca.chunk.js"
+  },
+  {
+    "revision": "4db16885b39b3b5cace1",
+    "url": "/reactportfolio/static/js/runtime-main.c29e04f1.js"
+  }
+]);
